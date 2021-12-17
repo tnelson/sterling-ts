b059af32e2d6e851f026704090ba64ca7e5f9bb4
Tristan Dyer <atdyer@ncsu.edu>
2021-12-17 07:32:14 -0500

    Throttle split pane resize events

--- packages/sterling/src/components/SplitPane.tsx
+++ packages/sterling/src/components/SplitPane.tsx
@@ -1,6 +1,7 @@
 import { DragHandle } from '@/sterling-ui';
 import { BoxProps } from '@chakra-ui/react';
 import { FC, useCallback, useEffect, useMemo, useState } from 'react';
+import { throttle } from 'lodash';
 
 interface SplitPaneProps {
   initialWidth: number;
@@ -15,6 +16,7 @@ const SplitPane = (props: SplitPaneProps) => {
     props;
 
   const [active, setActive] = useState<boolean>(false);
+  const [offset, setOffset] = useState<number>(0);
   const [width, setWidth] = useState<number>(initialWidth);
 
   const leftLeft = useMemo(
@@ -23,14 +25,22 @@ const SplitPane = (props: SplitPaneProps) => {
   );
   const rightLeft = useMemo(() => leftLeft + width, [leftLeft, width]);
 
-  const onMouseDown = useCallback(() => {
-    window.getSelection()?.empty();
-    setActive(true);
-  }, [setActive]);
-  const onMouseMove = useCallback(
+  const onMouseDown = useCallback(
     (event) => {
-      if (active) setWidth(event.clientX - leftPad);
+      window.getSelection()?.empty();
+      const rect = event.target.getBoundingClientRect();
+      setOffset(event.clientX - rect.left);
+      setActive(true);
     },
+    [setActive]
+  );
+  const onMouseMove = useCallback(
+    throttle((event) => {
+      if (active) {
+        window.getSelection()?.empty();
+        setWidth(event.clientX - leftPad + (8 - offset));
+      }
+    }, 10),
     [active, leftPad, setWidth]
   );
   const onMouseUp = useCallback(() => {
