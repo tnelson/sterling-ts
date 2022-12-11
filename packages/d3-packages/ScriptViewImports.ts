de912e7d0cc335f497db150665b58be6e531471c
bove1 <90574319+bove1@users.noreply.github.com>
2022-12-11 16:15:45 -0500

    minimal changes'

--- packages/d3-packages/ScriptViewImports.ts
+++ packages/d3-packages/ScriptViewImports.ts
@@ -6,9 +6,11 @@ import {Stage} from "./Stage"
 import { TextBox } from "./Textbox"
 import { VisualObject } from "./VisualObject"
 import {Line} from "./Line"
+import { Grid } from "./Grid"
 
 const scriptViewImports = [
     {name: "Pane", value: Pane},
+    {name: "Grid", value: Grid},
     {name: "Rectangle", value: Rectangle},
     {name: "Circle", value: Circle},
     {name: "Shape", value: Shape},
