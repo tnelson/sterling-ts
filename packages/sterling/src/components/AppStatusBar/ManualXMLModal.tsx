6eb23985604763537cb56188e09591b6af04e408
Tim Nelson <tn@cs.brown.edu>
2023-05-22 13:55:55 -0700

    feat: add XML from file

--- packages/sterling/src/components/AppStatusBar/ManualXMLModal.tsx
+++ packages/sterling/src/components/AppStatusBar/ManualXMLModal.tsx
@@ -1,5 +1,7 @@
-import { Button, FormControl, FormLabel, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Textarea, useToast } from '@chakra-ui/react';
-import { useRef } from 'react';
+import { Button, FormControl, FormLabel, Modal, ModalBody, ModalCloseButton, 
+         ModalContent, ModalFooter, ModalHeader, ModalOverlay, Textarea, 
+         useToast, Input, Divider, Center, Spacer } from '@chakra-ui/react';
+import { useRef, useState } from 'react';
 import { dataReceived } from 'sterling-connection/src/actions';
 import { parseAlloyDatum } from 'sterling-connection/src/parse/alloy';
 import store from 'sterling/src/state/store';
@@ -10,7 +12,7 @@ interface ManualXMLModelProps {
 }
 
 export const TEXT_manual_entry_modal_add_button = 'Add Datum'
-export const TEXT_manual_entry_modal_textarea_label = 'XML Datum'
+export const TEXT_manual_entry_modal_textarea_label = 'XML datum string to add'
 
 const placeholderXML = `
 <instance bitwidth= "4" maxseq= "-1" command= "example-run" filename= "no-filename.als" version= "1.5.0">
@@ -27,9 +29,16 @@ const placeholderXML = `
  * to the current datum list.
  */
 export function ManualXMLModal({isOpen, onClose}: ManualXMLModelProps) {  
-  const initialRef: React.MutableRefObject<HTMLTextAreaElement | null> = useRef(null)
+  /** Reference for the paste-in-XML text area */
+  const initialRef = useRef<HTMLTextAreaElement | null>(null)
+  /** Controlled value in the paste-in-XML text area */
+  let [xmlValue, setXMLValue] = useState('')
+
   const toast = useToast()
 
+  /** Reference for the load-from-file hidden input */
+  const inputFileRef = useRef<HTMLInputElement | null>(null);
+ 
   return (
     <>
       <Modal
@@ -38,12 +47,23 @@ export function ManualXMLModal({isOpen, onClose}: ManualXMLModelProps) {
       >
         <ModalOverlay />
         <ModalContent maxW="70rem" >
-          <ModalHeader>Input Alloy-style XML datum:</ModalHeader>
+
+          <ModalHeader><Center>Input Alloy-style XML datum (paste directly or load from file)</Center></ModalHeader>
           <ModalCloseButton />
+
           <ModalBody pb={6}>
+            <Button onClick={() => inputFileRef.current?.click()}>
+              Click to add XML from file
+            </Button> ...or simply paste in an XML datum below. Then click the "Add Datum" button.
+            <Spacer h='1rem' />
+            <Divider orientation='horizontal' mx={5} />
+            <Spacer h='1rem' />
             <FormControl>
               <FormLabel>{TEXT_manual_entry_modal_textarea_label}</FormLabel>
-              <Textarea minH="20rem" ref={initialRef} placeholder={placeholderXML} />
+              <Textarea minH="20rem" ref={initialRef} placeholder={placeholderXML} 
+              value={xmlValue}
+              onChange={e => setXMLValue(e.target.value)}
+              />
             </FormControl>
           </ModalBody>
 
@@ -63,7 +83,7 @@ export function ManualXMLModal({isOpen, onClose}: ManualXMLModelProps) {
                       : prev
                 }, 
                 0)
-              console.log(nextIDInt)
+              //console.log(nextIDInt)
               
               try {
                 store.dispatch(dataReceived(
@@ -93,6 +113,25 @@ export function ManualXMLModal({isOpen, onClose}: ManualXMLModelProps) {
                 onClose()
             }}
             >{TEXT_manual_entry_modal_add_button}</Button>
+
+            {/* Hidden input for file upload */}
+            <Input type='file' 
+                   ref={inputFileRef} 
+                   style={{display: 'none'}}
+                   onChange={e => {
+                      const reader = new FileReader()
+                      reader.onload = async (e) => { 
+                        if(e.target?.result)
+                          if(e.target.result instanceof ArrayBuffer) {
+                            setXMLValue(e.target.result.toString())
+                          } else {
+                            setXMLValue(e.target.result)
+                          }
+                      }
+                      if(e.target?.files && e.target.files.length > 0)
+                        reader.readAsText(e.target.files[0])
+                   }}
+                   />
           </ModalFooter>
         </ModalContent>
       </Modal>
