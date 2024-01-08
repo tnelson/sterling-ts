8803c7671219dddff484c75f5640792d7b7325ae
Sidney3 <sidneyarthurlevine1@hotmail.com>
2024-01-08 10:06:44 -0800

    updating d3lib tests according to new changes

--- packages/d3-packages/d3lib-demonstrations/conflictingmasksexample.js
+++ packages/d3-packages/d3lib-demonstrations/conflictingmasksexample.js
@@ -9,7 +9,7 @@ const stage = new Stage()
 
 
 
-const rect1 = new Rectangle({coords: {x: 50, y:50}, width: 400, height: 400})
+const rect1 = new Rectangle({center: {x: 50, y:50}, width: 400, height: 400})
 
 
 
@@ -18,12 +18,11 @@ rect1.addMask({top_left: {x:0,y:0}, bottom_right: {x:100,y:100}})
 rect1.addMask({top_left: {x:150,y:150}, bottom_right: {x:300, y:300}})
 
 
-const rect2 = new Rectangle({coords: {x:50, y:500}, width: 400, height:400})
+const rect2 = new Rectangle({center: {x:50, y:500}, width: 400, height:400})
 rect2.addMask({top_left: {x:0, y:0}, bottom_right: {x:300,y:300}})
 //proof that masks don't overlap!
 
-
-rect2.addMask({top_left: {x:75,y:505}, bottom_right: {x:100,y:700}})
+rect2.addMask({top_left: {x:75,y:200}, bottom_right: {x:100,y:700}})
 
 
 
