f8b18658c1b4acb90d8138eaf99ad6c622373b9a
Tim Nelson <tbnelson@gmail.com>
2025-01-09 10:02:12 -0500

    pre-populate generator selection on meta received

--- packages/sterling/src/state/ui/uiExtraReducers.ts
+++ packages/sterling/src/state/ui/uiExtraReducers.ts
@@ -16,6 +16,13 @@ function metaReceived(state: UiState, action: PayloadAction<ProviderMeta>) {
       }
     });
     state.mainView = state.availableViews[0];
+
+    // If the provider listed its generators, default to whatever it listed first.
+    // This will match the initial loading behavior of the Select drop-down component
+    // in the explorer pane. 
+    if(meta.generators && meta.generators.length > 0) {
+      state.selectedGenerator = meta.generators[0]
+    }
   }
 }
 
